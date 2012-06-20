d93ccffb651f2eb7556d9f10611ef360c64877ff
Jonas Hansen <hr.jonas.hansen@gmail.com>
2012-06-20 16:43:29 +0200

    1.0

--- serializer.cpp
+++ serializer.cpp
@@ -49,13 +49,40 @@ void serializer::deserialize_signal(std::vector<uint8_t>& serialized_data)
 //    std::cout << "inside deserialize function \n";
     uint32_t next_buffer_size;
     uint32_t index = 0;
+    int frames = 0;
+
+    uint8_t tmp[8];
+    memcpy(tmp, &serialized_data[index], 8);
+    for (int n = 0; n < 8; n++)
+    {
+    	for (int t = 0; t < 8; t++)
+    	{
+    		if (tmp[n] & (1 << t))
+			    std::cout << "1";
+			else
+			    std::cout << "0";
+			if (t %4 == 3)
+				std::cout << ".";
+		}
+	}
+	std::cout << std::endl;
     while(index < serialized_data.size()-1)
     {
 //        std::cout << "inside loop\n";
         memcpy(&next_buffer_size, &serialized_data[index],sizeof(uint32_t));
         // If next buffer is longer than remaining data in vector, an error has occured: break from loop.
-//        std::cout << "length of next buffer is " << next_buffer_size;
-        if (next_buffer_size > serialized_data.size()-index || next_buffer_size == 0) break;
+        std::cout << "length of " << frames << " buffer is " << next_buffer_size << std::endl;
+        if (next_buffer_size > serialized_data.size()-index)
+        {
+        	std::cout << "Out of bounds. Size: " << serialized_data.size()*1 << " index: " << index*1 << " Adr: " << (long)(&serialized_data[0]) << std::endl;
+            break;
+        }
+        if ( next_buffer_size == 0 )
+        {
+        	std::cout << "Zero-padded. Size: " << serialized_data.size()*1 << " index: " << index*1 << " Adr: " << (long)(&serialized_data[0]) << std::endl;
+            break;
+        }
+        frames++;
     	if (index)
     		Pframes++;//Pframes_Received++;
     	else
@@ -66,6 +93,7 @@ void serializer::deserialize_signal(std::vector<uint8_t>& serialized_data)
 //        std::cout << "just after signal\n";
         index += next_buffer_size;
     }
+    //std::cout << "Frames: " << frames << std::endl;
 }
 
 void serializer::deserialize_signal(uint8_t* data_ptr, uint32_t size)
